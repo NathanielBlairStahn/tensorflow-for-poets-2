d670d2de51ba48b0d985c64689027b6e14679195
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-01-22 16:48:25 -0800

    add retrained_graphs/ directory to retrain_template.sh

--- scripts/retrain_template.sh
+++ scripts/retrain_template.sh
@@ -8,7 +8,7 @@ python -m scripts.retrain \
   --how_many_training_steps=500 \
   --model_dir=tf_files/models/ \
   --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
-  --output_graph=tf_files/tree_graph.pb \
-  --output_labels=tf_files/tree_labels.txt \
+  --output_graph=tf_files/retrained_graphs/tree_graph.pb \
+  --output_labels=tf_files/retrained_graphs/tree_labels.txt \
   --architecture="${ARCHITECTURE}" \
   --image_dir=tf_files/tree_photos
